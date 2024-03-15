553a3439c191062311836fcf7035f897f9790d78
Zuka <zulanovicomar77@gmail.com>
2024-03-15 01:39:07 +0100

    Added mongoDB connection (not tested)

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,34 +1,28 @@
 import { Request, Response } from "express";
-import { User } from "../models/user";
+import { Collection } from "mongodb";
 
+export const registerUser = async (req: Request, res: Response, db: any) => {
+    try {
+        const { username, email, password } = req.body;
 
-export const registerUser = async (req: Request, res: Response) => {
-  try {
-    const { username, email, password } = req.body;
+        // Insert user data into MongoDB
+        const usersCollection: Collection = db.collection("users");
+        await usersCollection.insertOne({ username, email, password });
 
-    // validaja passworda
-    if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}/.test(password)) {
-      return res.status(400).json({ message: "Password must contain at least one lowercase letter, one uppercase letter, one digit, and be at least 8 characters long." });
+        res.status(201).json({ message: "User registered successfully." });
+    } catch (error) {
+        console.error("Error registering user:", error);
+        res.status(500).json({ message: "Server error" });
     }
-
-    // validacja emaila
-    if (!/\S+@\S+\.\S+/.test(email)) {
-      return res.status(400).json({ message: "Invalid email format." });
-    }
-    res.status(201).json({ message: "User registered successfully." });
-  } catch (error) {
-    console.error("Error registering user:", error);
-    res.status(500).json({ message: "Server error" });
-  }
 };
 
-
-export const getAllUsers = async (req: Request, res: Response) => {
-  try {
-    const users: User[] = [];
-    res.json(users);
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ message: "Server error" });
-  }
+export const getAllUsers = async (req: Request, res: Response, db: any) => {
+    try {
+        const usersCollection: Collection = db.collection("users");
+        const users = await usersCollection.find().toArray();
+        res.json(users);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ message: "Server error" });
+    }
 };
