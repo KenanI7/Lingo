c7fe007fda026a3034ba9b2c82b8dc2636ff7e4c
Zuka <zulanovicomar77@gmail.com>
2024-03-15 01:43:50 +0100

    fixed index.ts file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,33 @@
 import express from "express";
 import { registerUser } from "./controllers/userController";
 import { getAllUsers } from "./controllers/userController";
+import { MongoClient, Collection } from 'mongodb';
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/api/users", registerUser);
-app.get("/api/users", getAllUsers); 
+// Define MongoDB connection parameters
+const uri = 'mongodb+srv://LingoAdmin:2H9ixYwdHwvmUcy6@lingo.4dysqby.mongodb.net/';
+const dbName = 'your-database-name'; // Specify your database name here
 
+// Create a new MongoClient instance
+const client = new MongoClient(uri);
+
+// Connect to MongoDB
+async function connectToMongoDB() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        // Pass the database instance to the userRoutes
+        const db = client.db(dbName);
+        app.use("/api", userRoutes(db));
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+connectToMongoDB();
 
 
 app.get("/", (req, res) => {
